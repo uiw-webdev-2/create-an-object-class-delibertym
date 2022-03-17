040493ba51f689df17a7f55011c4f0b816d7c556
delibertym <89564178+delibertym@users.noreply.github.com>
2022-03-17 18:13:31 -0500

    Add files via upload

---
+++ newobjclass.js
@@ -0,0 +1,12 @@
+class Cars {
+    constructor(vehicle, brand){
+        this.vehicle = vehicle;
+        this.brand = brand;
+    }
+}
+
+var carOne = new Cars('Mercedes', 'black');
+var carTwo = new Cars('BMW', 'white');
+
+console.log(carOne);
+console.log(carTwo);
